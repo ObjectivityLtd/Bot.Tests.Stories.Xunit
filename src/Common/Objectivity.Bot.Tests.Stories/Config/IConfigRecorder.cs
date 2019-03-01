f1776960387c347f068070e4887aa629564753c7
Weronika Tobor <wtobor@objectivity.co.uk>
2019-03-01 15:38:08 +0100

    [ICIP-56] added configuration for channelId

---
+++ src/Common/Objectivity.Bot.Tests.Stories/Config/IConfigRecorder.cs
@@ -0,0 +1,9 @@
+﻿namespace Objectivity.Bot.Tests.Stories.Config
+{
+    using Recorder;
+
+    public interface IConfigRecorder<T>
+    {
+        IStoryRecorder<T> UseChannel(string channelId);
+    }
+}
