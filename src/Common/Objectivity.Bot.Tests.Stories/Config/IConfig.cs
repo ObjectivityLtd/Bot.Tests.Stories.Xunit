d005f56ec275a5dd2d4e118b52313cad88f6d7ae
Weronika Tobor <wtobor@objectivity.co.uk>
2019-03-01 15:38:08 +0100

    working on config class with UseChannel method

---
+++ src/Common/Objectivity.Bot.Tests.Stories/Config/IConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Objectivity.Bot.Tests.Stories.Config
+{
+    using Recorder;
+
+    public interface IConfig<T>
+    {
+        IStoryRecorder<T> UseChannel(string channelId);
+    }
+}
--- src/Common/Objectivity.Bot.Tests.Stories/Recorder/IStoryRecorder.cs
+++ src/Common/Objectivity.Bot.Tests.Stories/Recorder/IStoryRecorder.cs
@@ -1,9 +1,12 @@
 ﻿namespace Objectivity.Bot.Tests.Stories.Recorder
 {
+    using Config;
     using StoryModel;
 
     public interface IStoryRecorder<T>
     {
+        IConfig<T> Config { get; }
+
         IBotRecorder<T> Bot { get; }
 
         IUserRecorder<T> User { get; }
