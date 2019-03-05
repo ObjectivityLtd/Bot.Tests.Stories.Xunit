9e109b479a63d74ee1edf9cbe64019b15e222b36
Weronika Tobor <wtobor@objectivity.co.uk>
2019-03-05 14:58:25 +0100

    merge with feature/channelId; updated configuration variable

--- src/Common/Objectivity.Bot.Tests.Stories/Configuration/Configuration.cs
+++ src/Common/Objectivity.Bot.Tests.Stories/Configuration/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿namespace Objectivity.Bot.Tests.Stories.Configuration
 {
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
     using Recorder;
 
     public class Configuration<T> : IConfiguration, IConfigurationRecorder<T>
@@ -11,7 +13,16 @@
 
         public string ChannelId { get; set; } = "Test";
 
+        public ServiceCollection Services { get; } = new ServiceCollection();
+
         protected StoryRecorderBase<T> StoryRecorder { get; }
+
+        public IStoryRecorder<T> RegisterService(Action<ServiceCollection> registration)
+        {
+            registration?.Invoke(this.Services);
+
+            return this.StoryRecorder;
+        }
 
         public IStoryRecorder<T> UseChannel(string channelId)
         {
--- src/Common/Objectivity.Bot.Tests.Stories/Configuration/IConfiguration.cs
+++ src/Common/Objectivity.Bot.Tests.Stories/Configuration/IConfiguration.cs
@@ -1,7 +1,11 @@
 ﻿namespace Objectivity.Bot.Tests.Stories.Configuration
 {
+    using Microsoft.Extensions.DependencyInjection;
+
     public interface IConfiguration
     {
         string ChannelId { get; set; }
+
+        ServiceCollection Services { get; }
     }
 }
