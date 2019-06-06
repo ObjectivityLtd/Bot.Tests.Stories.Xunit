640d3854b8df4f7374f0090656282f7da88e7839
Mateusz Pustelnik <mpustelnik@objectivity.co.uk>
2019-06-06 19:37:39 +0200

    Added event assertion feature test samples

---
+++ src/V4/Objectivity.Bot.Tests.Stories.Xunit.V4.DemoBot/Dialogs/Prompts/TestAuthPrompt.cs
@@ -0,0 +1,43 @@
+﻿namespace Objectivity.Bot.Tests.Stories.Xunit.V4.DemoBot.Dialogs.Prompts
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Schema;
+
+    public class TestAuthPrompt : ActivityPrompt
+    {
+        public const string AuthRequestEventName = "tokens/request";
+        public const string AuthResponseEventName = "tokens/response";
+
+        public TestAuthPrompt(string dialogId, PromptValidator<Activity> validator)
+            : base(dialogId, validator)
+        {
+        }
+
+        protected override Task<PromptRecognizerResult<Activity>> OnRecognizeAsync(
+            ITurnContext turnContext,
+            IDictionary<string, object> state,
+            PromptOptions options,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var result = new PromptRecognizerResult<Activity>();
+            var activity = turnContext.Activity;
+
+            if (activity.Type == ActivityTypes.Event)
+            {
+                var ev = activity.AsEventActivity();
+
+                if (ev.Name == AuthResponseEventName)
+                {
+                    result.Succeeded = true;
+                    result.Value = turnContext.Activity;
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ src/V4/Objectivity.Bot.Tests.Stories.Xunit.V4.DemoBot/Dialogs/TestEventDialog.cs
@@ -0,0 +1,61 @@
+﻿namespace Objectivity.Bot.Tests.Stories.Xunit.V4.DemoBot.Dialogs
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Schema;
+    using Objectivity.Bot.Tests.Stories.Xunit.V4.DemoBot.Dialogs.Prompts;
+
+    public class TestEventDialog : ComponentDialog
+    {
+        private const string MainDialogId = nameof(MainDialogId);
+        private const string AuthDialogId = nameof(AuthDialogId);
+
+        public TestEventDialog()
+            : base(nameof(TestEventDialog))
+        {
+            WaterfallStep[] steps =
+            {
+                this.RequestAuthorizationAsync,
+                this.ShowAuthorizedAsync,
+            };
+
+            this.AddDialog(new WaterfallDialog(nameof(MainDialogId), steps));
+            this.AddDialog(new TestAuthPrompt(nameof(AuthDialogId), this.TokenResponseValidatorAsync));
+
+            this.InitialDialogId = nameof(MainDialogId);
+        }
+
+        protected async Task<bool> TokenResponseValidatorAsync(PromptValidatorContext<Activity> pc, CancellationToken cancellationToken)
+        {
+            var activity = pc.Recognized.Value;
+
+            if (activity == null || activity.Type != ActivityTypes.Event)
+            {
+                await pc.Context.SendActivityAsync("You're unauthorized", cancellationToken: cancellationToken);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private async Task<DialogTurnResult> RequestAuthorizationAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var authEventReply = stepContext.Context.Activity.CreateReply();
+            authEventReply.Type = ActivityTypes.Event;
+            authEventReply.Name = TestAuthPrompt.AuthRequestEventName;
+
+            await stepContext.Context.SendActivityAsync(authEventReply, cancellationToken);
+
+            return await stepContext.PromptAsync(nameof(AuthDialogId), new PromptOptions(), cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ShowAuthorizedAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("You're authorized", cancellationToken: cancellationToken);
+
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+    }
+}
