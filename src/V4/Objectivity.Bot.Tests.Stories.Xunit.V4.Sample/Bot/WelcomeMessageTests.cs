b80dc994ca195fd24390fc1ee5f65f005e067d2b
Mateusz Pustelnik <mpustelnik@objectivity.co.uk>
2019-06-13 15:00:20 +0200

    Added sample for ConversationUpdateMembers setting

--- src/V4/Objectivity.Bot.Tests.Stories.Xunit.V4.Sample/Bot/WelcomeMessageTests.cs
+++ src/V4/Objectivity.Bot.Tests.Stories.Xunit.V4.Sample/Bot/WelcomeMessageTests.cs
@@ -1,14 +1,27 @@
 ﻿namespace Objectivity.Bot.Tests.Stories.Xunit.V4.Sample.Bot
 {
     using System.Threading.Tasks;
+    using Extensions;
     using global::Xunit;
+    using Stories.Core;
 
     public class WelcomeMessageTests : DemoBotTestBase
     {
         [Fact]
-        public async Task ConversationInitiated_PlayStoryIsCalled_MustShowWelcomeMessage()
+        public async Task BotJoinedConversation_PlayStoryIsCalled_MustNotShowWelcomeMessage()
         {
             var story = this.Record
+                .Configuration.SetConversationUpdateMembers(ChannelId.Bot)
+                .Rewind();
+
+            await this.Play(story);
+        }
+
+        [Fact]
+        public async Task UserJoinedConversation_PlayStoryIsCalled_MustShowWelcomeMessage()
+        {
+            var story = this.Record
+                .Configuration.SetConversationUpdateMembers(ChannelId.User)
                 .Bot.Says("Welcome to demo bot")
                 .Rewind();
 
