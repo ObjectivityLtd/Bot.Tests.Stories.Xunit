5a19344397850ca005f29f4edf184ba2dc8ef95d
Mateusz Pustelnik <mpustelnik@objectivity.co.uk>
2019-06-17 07:30:21 +0200

    Changed user access token configuration type

---
+++ src/V4/Objectivity.Bot.Tests.Stories.Xunit.V4/Core/UserAccessTokenConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Objectivity.Bot.Tests.Stories.Xunit.V4.Core
+{
+    public class UserAccessTokenConfiguration
+    {
+        public string ConnectionName { get; set; }
+
+        public string ChannelId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Token { get; set; }
+
+        public string MagicCode { get; set; }
+    }
+}
